043b320c97c67a80e1f92ba69b1ee8b67882ca40
Sanjula Madurapperuma <sanjula99@gmail.com>
2021-11-22 09:35:12 +0530

    Refactor code

--- src/main/java/org/wso2/oidc/custom/claim/internal/PermissionClaimHandlingComponent.java
+++ src/main/java/org/wso2/oidc/custom/claim/internal/PermissionClaimHandlingComponent.java
@@ -1,48 +1,10 @@
 package org.wso2.oidc.custom.claim.internal;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.user.core.service.RealmService;
-import org.wso2.oidc.custom.claim.OIDCPermissionClaimHandler;
 
 public class PermissionClaimHandlingComponent {
 
-    private static Log log = LogFactory.getLog(PermissionClaimHandlingComponent.class);
-
-//    protected void activate(ComponentContext ctxt) {
-//
-//        try {
-//            log.info("Carbon Custom Claim Handler activated successfully.");
-//        } catch (Exception e) {
-//            log.error("Failed to activate Carbon Custom Claim Handler ", e);
-//        }
-//    }
-//
-//    protected void deactivate(ComponentContext ctxt) {
-//
-//        if (log.isDebugEnabled()) {
-//            log.debug("Carbon Custom Claim Handler is deactivated ");
-//        }
-//    }
-//
-//    protected void setRealmService(RealmService realmService) {
-//
-//        PermissionClaimHandlingComponent.realmService = realmService;
-//        if (log.isDebugEnabled()) {
-//            log.debug("RealmService is set in the Carbon Custom Claim Handler bundle");
-//        }
-//    }
-//
-//    protected void unsetRealmService(RealmService realmService) {
-//
-//        PermissionClaimHandlingComponent.realmService = null;
-//        if (log.isDebugEnabled()) {
-//            log.debug("RealmService is unset in the Carbon Custom Claim Handler bundle");
-//        }
-//    }
-//
     public static RealmService getRealmService() {
 
         RealmService realmService =
